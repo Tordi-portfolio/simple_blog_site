8a6df8e1c3cb62e1410bf3d503f0694b421c211f
Tordi-portfolio <164408531+Tordi-portfolio@users.noreply.github.com>
2026-05-28 22:20:58 +0100

    Add files via upload

---
+++ accounts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length=300)
+
+    image = models.ImageField(upload_to='blog_images/')
+
+    content = models.TextField()
+
+    author = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
---
+++ accounts/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        # check if username already exists
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists')
+            return redirect('register')
+        # create user
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        user.save()
+        messages.success(request, 'Account created successfully')
+        return redirect('login')
+    return render(request, 'register.html')
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('login')
+    return render(request, 'login.html')
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'Logged out successfully')
+    return redirect('login')
+
+
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+
+def blog(request):
+
+    blogs = Blog.objects.all().order_by('-created_at')
+
+    context = {
+        'blogs': blogs
+    }
+
+    return render(request, 'blog.html', context)
+
+
+def blog_details(request, id):
+
+    single_blog = get_object_or_404(Blog, id=id)
+
+    context = {
+        'blog': single_blog
+    }
+
+    return render(request, 'blog_details.html', context)
